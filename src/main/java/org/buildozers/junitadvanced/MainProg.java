42a38f9641d09f4614291b92771f71656e5bac44
FX ROBIN <francois-xavier.robin@capgemini.com>
2024-12-04 14:12:30 +0100

    adding SLF4J with logback-classic

--- src/main/java/org/buildozers/junitadvanced/MainProg.java
+++ src/main/java/org/buildozers/junitadvanced/MainProg.java
@@ -1,15 +1,18 @@
 package org.buildozers.junitadvanced;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.buildozers.junitadvanced.kata.heroes.Guild;
 import org.buildozers.junitadvanced.kata.heroes.Hero;
 import org.buildozers.junitadvanced.kata.heroes.HeroFactory;
 
+@Slf4j
 public class MainProg {
 
     public static void main(String... args) {
-        System.out.println(MainProg.getMessage());
+        log.info(MainProg.getMessage());
         Hero hero = HeroFactory.createHero(Guild.ROGUES, "Kevin", 0);
-        System.out.println(hero);
+        log.info(hero.toString());
     }
 
     public static String getMessage() {
